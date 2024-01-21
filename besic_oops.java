eff0b761a49592955f598ce1ca82b039947c6a9e
artist <azambari2001@gmail.com>
2024-01-21 13:48:43 +0530

    new

---
+++ besic_oops.java
@@ -0,0 +1,17 @@
+public class besic_oops {
+    //besic java oops
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        besic_oops peron1=new besic_oops();
+        besic_oops person2=new besic_oops();
+        peron1.name="Rahul";
+        peron1.age=22;
+        person2.name="neha";
+        person2.age=25;
+        System.out.println("name of person1 "+peron1.name+" age "+peron1.age);
+        System.out.println("name of person2 "+person2.name+" age "+person2.age);
+
+    }
+}
